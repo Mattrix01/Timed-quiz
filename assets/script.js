1d7469616c2c4771243fa99a12e0dc268fb1bc2b
Mattrix01 <emjay.knight@gmail.com>
2021-10-05 19:17:32 +0100

    Dynamic HTML added and timer in place

--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,17 @@ const choice4 = document.querySelector("#answer4");
 
 let chosenquestion = [];
 let answers = [];
+// questions array which questions im on:
 
-let questions = [
-    {
-        question: "What the question is?"
-        option1: '1',
-        option2: '2',
-        option3: '3',
-        option4: '4',
-    },
-]
+// let questions = [
+//     {
+//         question: "What the question is?"
+//         option1: '1',
+//         option2: '2',
+//         option3: '3',
+//         option4: '4',
+//     },
+// ]
 
 // timer
 let counter = 50;
@@ -25,12 +26,9 @@ let counter = 50;
 const counterSpan = document.querySelector("#counter");
 const counterDiv = document.querySelector("#counter-div");
 
-// execute every second "timerTick"
-const timer = setInterval(timerTick, 1000);
-
 const timerTick = function () {
   if (counter < 0) {
-    console.log("Out of time");
+    // console.log("Out of time");
     clearInterval(timer);
   } else {
     counterSpan.textContent = counter;
@@ -38,7 +36,40 @@ const timerTick = function () {
   }
 };
 
+// execute every second "timerTick"
+const timer = setInterval(timerTick, 1000);
 
 // const timerTick = function () {
 //   console.log(counter);
 // };
+
+let questionTemplate = `
+<h1>Question 1</h1>
+        <p id="questions-tag">
+          What is the first question?
+        </p>
+        <a id="answer1" class="button">Answer 1</a>
+        <a id="answer2" class="button">Answer 2</a>
+        <a id="answer3" class="button">Answer 3</a>
+        <a id="answer4" class="button">Answer 4</a>`;
+
+function nextQuestion() {
+  const questionsContainer = document.querySelector("#questions");
+  // bring the template in to this function and subtitute answers from the questions array.
+  // template literals https://www.w3schools.com/js/js_string_templates.asp
+  // put let question template
+  questionsContainer.innerHTML = questionTemplate;
+  // add event listeners for answer buttons
+  //
+}
+
+// function to check if correct button is pressed for the answer
+
+// event.target (look into this)
+
+// run everytime after that when the person gets the question right and thye ened to go next question, needs to be called gaain to enxt question
+// calling function
+nextQuestion();
+
+// template literal below
+// ${question.title};
