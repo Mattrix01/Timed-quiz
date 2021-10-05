d7c44da43edc9df8c56b4d541a41d1fe5b96fb27
Mattrix01 <emjay.knight@gmail.com>
2021-10-05 16:26:58 +0100

    targetting span for countdown and functions

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,40 @@ const choice2 = document.querySelector("#answer2");
 const choice3 = document.querySelector("#answer3");
 const choice4 = document.querySelector("#answer4");
 
-let question = [];
+let chosenquestion = [];
 let answers = [];
+
+let questions = [
+    {
+        question: "What the question is?"
+        option1: '1',
+        option2: '2',
+        option3: '3',
+        option4: '4',
+    },
+]
 
 // timer
 let counter = 50;
+
+// targets
+const counterSpan = document.querySelector("#counter");
+const counterDiv = document.querySelector("#counter-div");
+
+// execute every second "timerTick"
+const timer = setInterval(timerTick, 1000);
+
+const timerTick = function () {
+  if (counter < 0) {
+    console.log("Out of time");
+    clearInterval(timer);
+  } else {
+    counterSpan.textContent = counter;
+    counter -= 1;
+  }
+};
+
+
+// const timerTick = function () {
+//   console.log(counter);
+// };
