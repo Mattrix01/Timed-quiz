e6120f214a7bcacf310b160a7eba6941d34c8aef
Mattrix01 <emjay.knight@gmail.com>
2021-10-05 16:09:47 +0100

    Began javascript selectors and arrays setup

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// selectors
+const question = document.querySelector("#question");
+const choice1 = document.querySelector("#answer1");
+const choice2 = document.querySelector("#answer2");
+const choice3 = document.querySelector("#answer3");
+const choice4 = document.querySelector("#answer4");
+
+let question = [];
+let answers = [];
+
+// timer
+let counter = 50;
